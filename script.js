61ded1adfe72f3b0bc8b9767d02d58c78b934177
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-31 13:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+ function focusNext(input) {
+            const maxLength = parseInt(input.getAttribute('maxlength'));
+            if (input.value.length >= maxLength) {
+                const nextInput = input.nextElementSibling;
+                if (nextInput) {
+                    nextInput.focus();
+                }
+            }
+        }
+
+        function focusPrevious(input) {
+            if (event.key === "Backspace" && input.value.length === 0) {
+                const previousInput = input.previousElementSibling;
+                if (previousInput) {
+                    previousInput.focus();
+                }
+            }
+        }
